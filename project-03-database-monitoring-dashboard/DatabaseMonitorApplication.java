6550139b9e5912dabae370169282cf791e013e61
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-21 19:32:46 +0300

    Create DatabaseMonitorApplication.java

---
+++ project-03-database-monitoring-dashboard/DatabaseMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.alhanoof.databasemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseMonitorApplication.class, args);
+    }
+}
