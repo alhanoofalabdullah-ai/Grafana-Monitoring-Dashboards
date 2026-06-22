448311746fb42893d84af093a9585aa0ef6cf3bb
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 11:35:28 +0300

    Create IncidentMonitoringApplication.java

---
+++ project-17-incident-management-monitoring-dashboard/IncidentMonitoringApplication.java
@@ -0,0 +1,12 @@
+package com.alhanoof.incident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentMonitoringApplication.class, args);
+    }
+}
