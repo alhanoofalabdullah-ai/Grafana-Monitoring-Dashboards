ddecd72230594ba0bdad15ced43af6b5ec4d5f20
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 04:59:00 +0300

    Create LogMonitorApplication.java

---
+++ project-05-application-log-monitoring-dashboard/LogMonitorApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.logmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogMonitorApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                LogMonitorApplication.class,
+                args
+        );
+    }
+}
