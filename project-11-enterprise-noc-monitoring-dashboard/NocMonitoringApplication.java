fe13ac9bdb48819ef67ac126a82cf50e8824ce88
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 09:54:24 +0300

    Create NocMonitoringApplication.java

---
+++ project-11-enterprise-noc-monitoring-dashboard/NocMonitoringApplication.java
@@ -0,0 +1,12 @@
+package com.alhanoof.noc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NocMonitorApplication.class, args);
+    }
+}
