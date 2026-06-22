6b4846227d866cfb1b946f7d2660232c59f44bf9
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 09:17:14 +0300

    Create SocMonitorApplication.java

---
+++ project-09-soc-security-monitoring-dashboard/SocMonitorApplication.java
@@ -0,0 +1,16 @@
+package com.alhanoof.socmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocMonitorApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                SocMonitorApplication.class,
+                args
+        );
+    }
+}
